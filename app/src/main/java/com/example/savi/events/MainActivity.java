305455dbd231fd125b9725bd2e5851933ae82b9e
savi30 <savianubogdan@gmail.com>
2017-04-27 04:28:11 +0300

    feat: Add search function
    Added FloatingActionButton, overrode the OnClick() function to trigger the query in the Graph API

--- app/src/main/java/com/example/savi/events/MainActivity.java
+++ app/src/main/java/com/example/savi/events/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.savi.events;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
@@ -17,25 +21,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AccessToken accessToken=AccessToken.getCurrentAccessToken();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        GraphRequest request = GraphRequest.newGraphPathRequest(
-                accessToken,
-                "/search",
-                new GraphRequest.Callback() {
-                    @Override
-                    public void onCompleted(GraphResponse response) {
-                        Log.e(LOG_TAG,response.toString());
-                    }
-                });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        Bundle parameters = new Bundle();
-        parameters.putString("q", "Cluj");
-        parameters.putString("type", "event");
-        parameters.putString("limit", "1000");
-        parameters.putString("pretty", "200");
-        request.setParameters(parameters);
-        request.executeAsync();
+                AccessToken accessToken=AccessToken.getCurrentAccessToken();
+
+                GraphRequest request = GraphRequest.newGraphPathRequest(
+                        accessToken,
+                        "/search",
+                        new GraphRequest.Callback() {
+                            @Override
+                            public void onCompleted(GraphResponse response) {
+                                Log.e(LOG_TAG,response.toString());
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("q", "Cluj");
+                parameters.putString("type", "event");
+                parameters.putString("limit", "1000");
+                parameters.putString("pretty", "0");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+                Toast.makeText(MainActivity.this,"Graph API queryed",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
 
     }
 }
