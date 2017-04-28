fd4922bbe3b7a3521776ae5e21a6136d83bea44e
savi30 <savianubogdan@gmail.com>
2017-04-29 02:22:54 +0300

    feat: Add facebok login option. Remove email login option

--- app/src/main/java/com/example/savi/events/LoginActivity.java
+++ app/src/main/java/com/example/savi/events/LoginActivity.java
@@ -3,19 +3,26 @@ package com.example.savi.events;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.accountkit.AccessToken;
 import com.facebook.accountkit.Account;
 import com.facebook.accountkit.AccountKit;
 import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.LoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+
 /**
  * Created by savi on 27.04.2017.
  */
@@ -24,13 +31,38 @@ public class LoginActivity extends AppCompatActivity {
     private static final String LOG_TAG=LoginActivity.class.getName();
 
     private static final int APP_REQUEST_CODE =1;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton =(LoginButton) findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("email");
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<com.facebook.login.LoginResult>() {
+            @Override
+            public void onSuccess(com.facebook.login.LoginResult loginResult) {
+                launchMainActivity();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                String toastMsg=error.getMessage();
+                Toast.makeText(LoginActivity.this,toastMsg,Toast.LENGTH_LONG);
+            }
+        });
+
         AccessToken accessToken = AccountKit.getCurrentAccessToken();
-        if(accessToken!=null) {
+        com.facebook.AccessToken loginToken = com.facebook.AccessToken.getCurrentAccessToken();
+        if(accessToken!=null || loginToken!=null) {
             launchMainActivity();
         }
 
@@ -39,6 +71,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(final int requestCode,final int resultCode,final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+
         if(requestCode == APP_REQUEST_CODE) {
             AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
             if(loginResult.getError()!=null) {
@@ -61,17 +95,10 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(intent,APP_REQUEST_CODE);
     }
 
-
     public void onPhoneLogin(View view) {
         AppEventsLogger logger = AppEventsLogger.newLogger(this);
         logger.logEvent("onPhoneLogin");
         onLogin(LoginType.PHONE);
-    }
-
-    public void onEmailLogin(View view) {
-        AppEventsLogger logger = AppEventsLogger.newLogger(this);
-        logger.logEvent("onEmailLogin");
-        onLogin(LoginType.EMAIL);
     }
 
     private void launchMainActivity() {
